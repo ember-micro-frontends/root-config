66d13fd3a0807a3b6797b55323a892dd01ac727d
Rajasegar <rajasegar.c@gmail.com>
2020-12-19 05:44:02 +0530

    chore: add films mfe

--- src/ember-mfe-root-config.js
+++ src/ember-mfe-root-config.js
@@ -6,7 +6,7 @@ import {
 } from "single-spa-layout";
 import { loadEmberApp } from "single-spa-ember";
 
-const isLocal = false;
+const isLocal = true;
 const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
   loaders: {
     topNav: "<h1>Loading topnav</h1>",
@@ -58,10 +58,25 @@ const peopleApp = registerApplication(
   (location) => location.pathname.startsWith("/people")
 );
 
+const filmsDomain = isLocal
+  ? "http://localhost:4203/films"
+  : "https://ember-micro-frontends.github.io/films";
+const filmsApp = registerApplication(
+  "films",
+  () => {
+    const appName = "films";
+    const appUrl = `${filmsDomain}/assets/films.js`;
+    const vendorUrl = `${filmsDomain}/assets/vendor.js`;
+    return loadEmberApp(appName, appUrl, vendorUrl);
+  },
+  (location) => location.pathname.startsWith("/films")
+);
+
 const applications = [];
 applications.concat(navbarApp);
 applications.concat(planetsApp);
 applications.concat(peopleApp);
+applications.concat(filmsApp);
 
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
